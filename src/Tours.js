430111f0120de959c2bda0a8d442348645772481
AAdrianML <trabajo.aaml@gmail.com>
2022-01-12 22:35:18 -0700

    tours screen and tour child

--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours=[]}) => {
+
+
+  const listOfTours = tours.map((tour)=>{
+    //{tour} it gives me the possibility to access to all the tour data array
+      if (tour.id, tour.name, tour.info, tour.price) {
+        return <Tour key={tour.id} {...tour}/> 
+      }
+  })
+
+  return <section>
+          <div className='title'>
+            <h2>Ours tours</h2>
+            <div className="underline"></div>
+          </div>
+          <div>
+            {listOfTours}
+          </div>
+        </section>;
 };
 
 export default Tours;
